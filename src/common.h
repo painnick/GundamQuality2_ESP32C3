87feb448462b2b0c66853ceb1cc9901ddd6a8569
painnick <painnick@gmail.com>
2024-11-15 23:21:50 +0900

    REFACTORING.

---
+++ src/common.h
@@ -0,0 +1,14 @@
+//
+// Created by painnick on 2024-11-15.
+//
+
+#ifndef GUNDAMQUALITY2_ESP32C3_SRC_COMMON_H_
+#define GUNDAMQUALITY2_ESP32C3_SRC_COMMON_H_
+
+#include "esp_log.h"
+
+const int PIN_STEP_MOTOR_STEP = 3;
+const int PIN_STEP_MOTOR_ENABLE = 4;
+const int PIN_HALL_SENSOR = 5;
+
+#endif //GUNDAMQUALITY2_ESP32C3_SRC_COMMON_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
-const int STEP = 3;
-const int ENABLE = 4;
-const int HALL_SENSOR = 5;
+#include "common.h"
+
+#define MAIN_TAG "Main"
 
 const int steps_per_rev = 2048 /* 200 */;
 
@@ -14,37 +14,37 @@ void IRAM_ATTR InitPos() {
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(STEP, OUTPUT);
-  pinMode(ENABLE, OUTPUT);
+  ESP_LOGI(MAIN_TAG, "Start!");
+  pinMode(PIN_STEP_MOTOR_STEP, OUTPUT);
+  pinMode(PIN_STEP_MOTOR_ENABLE, OUTPUT);
 
-  gpio_set_direction((gpio_num_t) HALL_SENSOR, GPIO_MODE_INPUT);
-  attachInterrupt(HALL_SENSOR, InitPos, RISING);
+  gpio_set_direction((gpio_num_t) PIN_HALL_SENSOR, GPIO_MODE_INPUT);
+  attachInterrupt(PIN_HALL_SENSOR, InitPos, RISING);
 
-  digitalWrite(ENABLE, LOW);
+  digitalWrite(PIN_STEP_MOTOR_ENABLE, LOW);
   while (!initialized) {
-    digitalWrite(STEP, HIGH);
+    digitalWrite(PIN_STEP_MOTOR_STEP, HIGH);
     delayMicroseconds(1000);
-    digitalWrite(STEP, LOW);
+    digitalWrite(PIN_STEP_MOTOR_STEP, LOW);
     delayMicroseconds(1000);
   }
-  digitalWrite(ENABLE, HIGH);
-  Serial.println("Initialized");
+  digitalWrite(PIN_STEP_MOTOR_ENABLE, HIGH);
+  ESP_LOGI(MAIN_TAG, "Initialize the step motor.");
 
+  ESP_LOGD(MAIN_TAG, "Wait 3 seconds...");
   delay(1000 * 3);
 }
 
 void loop() {
-  Serial.println("Spinning Clockwise...");
-
-  digitalWrite(ENABLE, LOW);
+  digitalWrite(PIN_STEP_MOTOR_ENABLE, LOW);
   for (int i = 0; i < steps_per_rev; i++) {
-    digitalWrite(STEP, HIGH);
+    digitalWrite(PIN_STEP_MOTOR_STEP, HIGH);
     delayMicroseconds(800);
-    digitalWrite(STEP, LOW);
+    digitalWrite(PIN_STEP_MOTOR_STEP, LOW);
     delayMicroseconds(800);
   }
-  digitalWrite(ENABLE, HIGH);
+  digitalWrite(PIN_STEP_MOTOR_ENABLE, HIGH);
 
+  ESP_LOGD(MAIN_TAG, "Wait 3 seconds...");
   delay(1000 * 3);
 }
